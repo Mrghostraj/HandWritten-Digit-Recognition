ac16da9f229feef1eeb2b60fd6f6bd7c9ef6cce7
Randhir Kumar <86366172+Mrghostraj@users.noreply.github.com>
2025-09-26 17:17:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -40,4 +40,5 @@ async def predict(file: UploadFile = File(...)):
     return {"predicted_digit": digit}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
+
