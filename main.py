af3371fa58be9054cc1411123319e736f28bcfc8
Randhir Kumar <86366172+Mrghostraj@users.noreply.github.com>
2025-10-03 01:19:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, UploadFile, File
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 import numpy as np
 from PIL import Image
 import tensorflow as tf
-from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles
+import uvicorn
 import os
 
 # ✅ Load model
@@ -13,15 +12,6 @@ model = tf.keras.models.load_model("digit_model.keras")
 # ✅ Create app
 app = FastAPI()
 
-# ✅ CORS (for frontend access)
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
 # ✅ Preprocess image
 def preprocess_image(image: Image.Image):
     image = image.convert("L").resize((28, 28))  # grayscale, 28x28
@@ -29,7 +19,8 @@ def preprocess_image(image: Image.Image):
     img_array = img_array.reshape(1, 28, 28, 1)
     return img_array
 
-@app.get('/')
+# ✅ Serve index.html
+@app.get("/")
 def read_root():
     return FileResponse("index.html")
 
@@ -45,7 +36,9 @@ async def predict(file: UploadFile = File(...)):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
+
     
+
 
 
 
